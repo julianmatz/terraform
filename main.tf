054629591e4af9b41f0815b41dade2f89cd7208a
Julian Matz <jmatz@netlink.ie>
2023-06-23 04:01:26 +0100

    Add main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,58 @@ provider "aws" {
   alias = "eu-west-1"
   region = "eu-west-1"
 }
+
+data "aws_ami" "debian" {
+  most_recent = true
+  owners      = ["679593333241"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["debian-10-amd64-*"]
+  }
+}
+
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.debian.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "test"
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_type = "gp3"
+    volume_size = 10
+  }
+
+  vpc_security_group_ids = [aws_security_group.allow_http_https.id]
+}
+
+resource "aws_security_group" "allow_http_https" {
+  name        = "allow_http_https"
+  description = "Allow HTTP and HTTPS traffic"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
