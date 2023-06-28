4abd1765009ab613cd51179ed0ad4380f7e5b754
Julian Matz <jmatz@netlink.ie>
2023-06-28 02:45:56 +0100

    Update Terraform structure

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "main" {
+resource "aws_instance" "default" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
@@ -18,6 +18,30 @@ resource "aws_instance" "main" {
   }
 
   lifecycle {
-    prevent_destroy = var.prevent_destroy
+    prevent_destroy = false
+  }
+}
+
+resource "aws_instance" "persistent" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = var.storage_size
+    volume_type = "gp3"
+    throughput  = var.throughput
+  }
+
+  tags = {
+    Name        = var.instance_name
+    Environment = var.environment
+    Roles       = var.roles
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
