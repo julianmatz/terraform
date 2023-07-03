f6c937f29349f6a9dcfca9349d566f0660373579
Julian Matz <jmatz@netlink.ie>
2023-07-03 23:23:51 +0100

    Restructure Terraform plan

--- aws/modules/compute/main.tf
+++ aws/modules/compute/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.6.2"
+    }
+  }
+}
+
 resource "aws_instance" "default" {
   ami           = var.ami
   instance_type = var.instance_type
